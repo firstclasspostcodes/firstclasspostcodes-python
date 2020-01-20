3dcdf9ba3113e53ade0a905b8091d23e92123a6a
davidkelley <david.james.kelley@gmail.com>
2020-01-20 18:15:55 +0000

    style: 💄 fixing styles in test class

--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -1,12 +1,9 @@
 from firstclasspostcodes.configuration import Configuration
 
+
 class TestConfigurationClass:
     def setup_method(self):
         self.foo = 'bar'
 
     def test_one(self):
         assert self.foo == "bar"
-
-    # def test_two(self):
-    #     x = "hello"
-    #     assert hasattr(x, "check")
